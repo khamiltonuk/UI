9519c36a623217044fe255653de711c308002a5c
khamiltonuk <krisham87@hotmail.com>
2017-09-05 05:30:28 +0100

    add bootstrap, html loader and jumbotron

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,14 @@ const path = require('path');
 module.exports = {
     entry: path.resolve('./public/index.html'),
     devServer:{
+        //inline: true,
         contentBase:"./public",
         port:3000
+    },
+    module: {
+        loaders: [{
+          test: /\.html$/,
+          loader: "html-loader"
+        }]
     }
 };
