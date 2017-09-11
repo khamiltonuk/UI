5366ce7d6f9dbdc943387ea13fb55707ab0745ff
krihamilton <krihamilton@gumtree.com>
2017-09-11 19:05:47 +0100

    attempt transition with react

--- source/app.js
+++ source/app.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import Button from './bootstrap/button';
 import Jumbotron from './bootstrap/jumbotron';
+import Transition from 'react-transition-group/Transition'
 import './sourceapp.scss'
 
 export default class App extends React.Component{
 	render(){
-		return <Jumbotron className="-fluid text-xs-center custom"	containerFluid="true">
+		return <Transition
+			transitionName="app"
+			transitionEnter={true}
+			transitionEnterTimeout={500}
+			transitionLeaveTimeout={500}>
+			<Jumbotron className="-fluid custom"	containerFluid="true">
 				<h1>Building React.js User Interfaces</h1>
 				<p>with Bootstrap and SASS</p>
 				<p><Button className="-primary -secondary-outline"
 							href="http://02geek.com"
-							target="_blank" disabled/>
+							target="_blank" />
 						<Button className="-danger-outline -sm"
-										target="_blank" disabled/>
+										target="_blank" />
 				</p>
 				<Button className="-primary-outline -lg -block"/>
-			</Jumbotron>;
+			</Jumbotron></Transition>;
 
 	}
 }
